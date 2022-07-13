146d921fe330df5c893d21c9911f45e6c7f7abb4
Sam DeVore <sdevore@gmail.com>
2022-07-13 11:03:38 -0700

    chore: initial checkin

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import {homedir} from 'os'
+import {resolve} from 'path'
+import fs from 'fs';
 
+let host = 'btchnmoan.test'
 export default defineConfig({
     plugins: [
         laravel({
@@ -15,5 +19,41 @@ export default defineConfig({
                 },
             },
         }),
+        // https://freek.dev/2277-using-laravel-vite-to-automatically-refresh-your-browser-when-changing-a-blade-file
+        {
+            name: 'blade',
+            handleHotUpdate({ file, server }) {
+                if (file.endsWith('.blade.php')) {
+                    server.ws.send({
+                        type: 'full-reload',
+                        path: '*',
+                    });
+                }
+            },
+        }
     ],
+    server: detectServerConfig(host),
 });
+
+// https://freek.dev/2276-making-vite-and-valet-play-nice-together
+function detectServerConfig(host) {
+    let keyPath = resolve(homedir(), `.config/valet/Certificates/${host}.key`)
+    let certificatePath = resolve(homedir(), `.config/valet/Certificates/${host}.crt`)
+
+    if (!fs.existsSync(keyPath)) {
+        return {}
+    }
+
+    if (!fs.existsSync(certificatePath)) {
+        return {}
+    }
+
+    return {
+        hmr: {host},
+        host,
+        https: {
+            key: fs.readFileSync(keyPath),
+            cert: fs.readFileSync(certificatePath),
+        },
+    }
+}
